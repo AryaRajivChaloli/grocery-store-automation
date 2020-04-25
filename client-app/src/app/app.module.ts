36a6e8c7f89a312bc29ceb6cdbcef5b72593cb0e
Arya Rajiv Chaloli <45247246+AryaRajivChaloli@users.noreply.github.com>
2020-04-25 10:21:45 +0530

    added component

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputBoxComponent } from './input-box/input-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBoxComponent
   ],
   imports: [
     BrowserModule,
