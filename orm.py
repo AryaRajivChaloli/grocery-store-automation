2477ba4251189c5091c890bec1cb125c777881b2
Ashika Meryl Pinto <44089778+ashikapinto@users.noreply.github.com>
2020-05-04 22:52:31 +0530

    added 3 apis

--- orm.py
+++ orm.py
@@ -32,7 +32,7 @@ class product(db.Model):
 	__tablename__="product"
 	product_name=db.Column(db.String(20),nullable=False)
 	product_id=db.Column(db.String(10),primary_key=True)	# to be generated randomly
-	product_cost=db.Column(db.DECIMAL(5,2),nullable=False)
+	product_cost=db.Column(db.Integer(5,2),nullable=False)
 	return_by=db.Column(db.Date(),nullable=False)
 	#defining a one-to-many relationship between seller and product
 	product_seller=db.Column(db.String(10),db.ForeignKey("seller.seller_id"))
@@ -45,7 +45,7 @@ class inventory(db.Model):
 	sl_no=db.Column(db.Integer,primary_key=True)		#to be generated sequentially
 	prod_id=db.Column(db.String(10),db.ForeignKey("product.product_id"),nullable=False)
 	quantity=db.Column(db.Integer,nullable=False)
-	cost=db.Column(db.DECIMAL(10,2),nullable=False)		#quantity*product_cost
+	cost=db.Column(db.Integer(10,2),nullable=False)		#quantity*product_cost
 	expiry=db.Column(db.Date,nullable=False)
 	prod_relationship=relationship("product",backref = "inventory")
 
@@ -54,3 +54,38 @@ db.create_all()
 #seller1=seller.query.first()
 #seller1.products gives the list or products the seller has entered using the relation specified
 
+#given product id return all details
+@app.route('/api/product/product_home',methods=['POST'])
+def seller_create_user():
+    prod_id = request.json['product_id']
+    res = product.query.filter_by(product_id=prod_id).first()
+    response_dict={}
+    response_dict["product_name"]=res.product_name
+    response_dict["product_cost"]=res.product_cost
+    response_dict["return_by"]=res.product_name
+    response_dict["product_seller"]=res.product_seller
+    return jsonify(response_dict)
+
+#add to inventory
+@app.route('/api/update_inventory',methods=['POST'])
+def update_inventory():
+    prod_id = request.json['product_id']
+    cost = request.json['cost']
+    expiry = request.json['expiry']
+    quantity = request.json['quantity']
+    sl_no = len(inventory.query.all())+1
+    x = inventory(sl_no,prod_id,quantity,cost,expiry)
+    db.session.add(x)
+    db.session.commit()
+    status = "added"
+    return jsonify(data = status)
+
+#send store id get all product ids
+@app.route('/api/seller/get_all_products',methods=['POST'])
+def get_all_products():
+    seller_id=request.json['seller_id']
+    res = product.query.filter_by(product_seller=seller_id).all()
+    result=[i.product_id for i in res]
+    return jsonify(result)
+if __name__ == '__main__':
+    app.run(debug=True)
